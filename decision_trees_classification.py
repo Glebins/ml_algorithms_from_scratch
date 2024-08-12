27eb8c90a804b58bc0db4548a51b94ffebdd0be8
glebins <nedobuga.gd@gmail.com>
2024-08-12 22:06:15 +0300

    todo fit for DT

--- decision_trees_classification.py
+++ decision_trees_classification.py
@@ -22,26 +22,33 @@ def get_best_split(X: pd.DataFrame, y: pd.Series):
 
     best_params = {'col_name': None, 'split_value': None, 'ig': 0}
 
+    S_0 = get_entropy_of_col(y)
+
     for column_i in X:
-        delimiters = X[column_i].unique()
+        unique_values = sorted(X[column_i].unique())
+        delimiters = []
+        for i in range(len(unique_values) - 1):
+            mean_i = (unique_values[i] + unique_values[i + 1]) / 2
+            delimiters.append(mean_i)
 
         len_objects = len(X[column_i])
 
-        S_0 = get_entropy_of_col(X[column_i])
-
         for delim in delimiters:
-            basket_left = X[X[column_i] <= delim][column_i]
-            basket_right = X[X[column_i] > delim][column_i]
+            X_left = X[X[column_i] <= delim][column_i]
+            X_right = X[X[column_i] > delim][column_i]
 
-            if basket_right.empty or basket_left.empty:
+            y_left = y[X_left.index]
+            y_right = y[X_right.index]
+
+            if X_left.empty or X_right.empty:
                 continue
 
-            S_1 = get_entropy_of_col(basket_left)
-            S_2 = get_entropy_of_col(basket_right)
+            S_1 = get_entropy_of_col(y_left)
+            S_2 = get_entropy_of_col(y_right)
 
-            information_gain = S_0 - len(basket_left) / len_objects * S_1 - len(basket_right) / len_objects * S_2
+            information_gain = S_0 - len(y_left) / len_objects * S_1 - len(y_right) / len_objects * S_2
 
-            print(basket_left.tolist(), basket_right.tolist(), delim, S_0, S_1, S_0 - S_1 * len(basket_left) / len_objects)
+            print(delim, y_left.tolist(), y_right.tolist(), column_i, information_gain)
 
             if information_gain > best_params['ig']:
                 best_params['ig'] = information_gain
@@ -56,6 +63,19 @@ class MyTreeClf:
         self.max_depth = max_depth
         self.min_samples_split = min_samples_split
         self.max_leafs = max_leafs
+
+        self.leafs_cnt = 0
+
+    def fit(self, X: pd.DataFrame, y: pd.Series):
+        col_name, split_value, _ = get_best_split(X, y)
+
+        X_left = X[X[col_name] <= split_value]
+        X_right = X[X[col_name] > split_value]
+
+        y_left = y[X_left.index]
+        y_right = y[X_right.index]
+
+
 
     def __str__(self):
         res_str = f"{self.__class__.__name__} class: "
@@ -79,3 +99,34 @@ class MyTreeClf:
         res_str += ")"
 
         return res_str
+
+
+class Tree:
+    def __init__(self, col_name=None, split_value=None):
+        self.left = None
+        self.right = None
+
+        self.col_name = col_name
+        self.split_value = split_value
+
+    def add_node(self, is_left: bool):
+        if is_left:
+            self.left = Tree()
+        else:
+            self.right = Tree()
+
+    @property
+    def col_name(self):
+        return self.col_name
+
+    @col_name.setter
+    def col_name(self, col_name_new):
+        self.col_name = col_name_new
+
+    @property
+    def split_value(self):
+        return self.split_value
+
+    @split_value.setter
+    def split_value(self, split_value_new):
+        self.split_value = split_value_new
--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from decision_trees_classification import *
 np.random.seed(42)
 
 X = pd.DataFrame(np.round(np.random.random((5, 4)), 1))
-y = pd.Series(np.random.randint(0, 2, size=50))
+y = pd.Series(np.random.randint(0, 2, size=5))
 
 print(X, end="\n\n")
 
